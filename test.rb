29ea71e5e6de91c26cc154cc77c0984e2e07fa9f
Matt Neary <neary.matt@gmail.com>
2013-11-04 13:28:59 -0500

    test specs for Rackus

---
+++ test.rb
@@ -0,0 +1,8 @@
+require './rackus.rb'
+
+enum = Rackus.enum 'A', Rackus.join('A', Rackus.token(:enum))
+enum.register! :enum, enum
+p enum.test("C") == false
+p enum.test("A") == true
+p enum.test('AA') == true
+p enum.test('CB') == false
