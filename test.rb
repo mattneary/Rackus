8e0915381ca14631572561139c64873a37f8f606
Matt Neary <neary.matt@gmail.com>
2013-11-22 13:28:14 -0500

    DSL test syntax

--- test.rb
+++ test.rb
@@ -1,6 +1,6 @@
 require './rackus.rb'
 
-simple = And 'A', 'B'
+simple = (Const 'A') + (Const 'B')
 
 p simple.test('A') == false
 p simple.test('B') == false
@@ -9,7 +9,7 @@ p simple.read('AB') == ['A', 'B']
 p simple.test('ACB') == false
 p simple.test('ABC') == false
 
-enum = Or 'A', (And 'A', (Token :enum))
+enum = (Const 'A') | ((Const 'A') + (Token :enum))
 enum.register! :enum, enum
 
 p enum.test("C") == false
@@ -20,27 +20,27 @@ p enum.read('AA') == ["A", "A"]
 p enum.test('AB') == false
 p enum.test('CB') == false
 
-string = And 'A', 'B', 'C'
+string = (Const 'A') + (Const 'B') + (Const 'C')
 
 p string.test('ABC') == true
 p string.read('ABC') == ['A', 'B', 'C']
 p string.test('AB') == false
 p string.test('ABCD') == false
 
-regular = And (Token :a), (Token :b), (Token :a)
-regular.register! :a, (Rackus.const 'A')
+regular = (Token :a) + (Token :b) + (Token :a)
+regular.register! :a, (Const 'A')
 regular.register! :b, (Or 'B', (And 'B', (Token :b)))
 
 p regular.test('ABA') == true
 p regular.test('ABC') == false
 p regular.test('ACB') == false
 
-dslOr = (Rackus.const 'A') | (Rackus.const 'B') | (Rackus.const 'C')
+dslOr = (Const 'A') | (Const 'B') | (Const 'C')
 p dslOr.test('A') == true
 p dslOr.test('B') == true
 p dslOr.test('D') == false
 
-dslAnd = (Rackus.const 'A') + (Rackus.const 'B') + (Rackus.const 'C')
+dslAnd = (Const 'A') + (Const 'B') + (Const 'C')
 p dslAnd.test('ABC') == true
 p dslAnd.test('ACB') == false
 p dslAnd.test('ABD') == false
