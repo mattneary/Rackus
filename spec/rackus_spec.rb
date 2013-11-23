9a64a47d27f97a6e3c17590780928b4f86ee5428
Matt Neary <neary.matt@gmail.com>
2013-11-23 13:19:29 -0500

    rspec tests

---
+++ spec/rackus_spec.rb
@@ -0,0 +1,39 @@
+require '../rackus.rb'
+
+describe Rackus, "#test" do
+  it "verifies a basic constant" do
+    const = Const 'A'
+    const.test('B').should be false
+    const.test('A').should be true
+  end
+  it "verifies constant join" do
+    join = (Const 'A') + (Const 'B')
+    join.test('A').should be false
+    join.test('AC').should be false
+    join.test('AB').should be true
+  end
+  it "verifies a recursive chain" do
+    enum = (Const 'A') | ((Const 'A') + (Token :enum))
+    enum.register! :enum, enum
+    enum.test('C').should be false
+    enum.test('A').should be true
+    enum.test('AA').should be true
+    enum.test('AB').should be false
+    enum.test('AC').should be false
+  end
+  it "verifies multiple const join" do
+    multiple = (Const 'A') + (Const 'B') + (Const 'C')
+    multiple.test('A').should be false
+    multiple.test('AB').should be false
+    multiple.test('ABC').should be true
+    multiple.test('ABCD').should be false
+  end
+  it "verifies a regular language tree" do
+    regular = (Token :a) + (Token :b) + (Token :a)
+    regular.register! :a, (Const 'A')
+    regular.register! :b, ((Const 'B') | ((Const 'B') + (Token :b)))
+    regular.test('ABA').should be true
+    regular.test('ABC').should be false
+    regular.test('ACB').should be false
+  end
+end
